2509e68282c5c973dd97b5308100995a017bcade
Cleiton Viana <cleitonviana.2@outlook.com.br>
2023-06-21 18:43:12 -0300

    adicionado footer conteúdo no aside bem como algumas melhorias no código geral

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const menu_button_close = document.querySelector('.menu__button--close');
 const menu_mobile = document.querySelector('.menu__list--mobile');
 const menu_top = document.querySelector('.menu-top');
 const menu = document.querySelector('.menu');
-const aside = document.querySelector('.aside');
-const aside_button_open = document.querySelector('.side-bar__icon');
-const aside_button_close = document.querySelector('.aside__button');
 
 menu_button_open.addEventListener('click', () => {
    (menu_mobile.classList.contains('hidden')) ? menu_mobile.classList.remove('hidden') : menu_mobile.classList.add('hidden')
@@ -14,6 +11,12 @@ menu_button_open.addEventListener('click', () => {
 menu_button_close.addEventListener('click', () => {
    (menu_mobile.classList.contains('hidden')) ? menu_mobile.classList.remove('hidden') : menu_mobile.classList.add('hidden')
 })
+
+
+const aside = document.querySelector('.aside');
+const aside_button_open = document.querySelector('.side-bar__icon');
+const aside_button_close = document.querySelector('.aside__button');
+
 
 aside_button_open.addEventListener('click', () => {
    aside.classList.remove('hidden');
@@ -30,6 +33,28 @@ aside_button_close.addEventListener('click', () => {
    menu.classList.remove('hidden');
    menu.classList.add('menu--display');
 })
+
+
+const input_checkbox = document.querySelector('#checkbox_creator');
+const check_box = document.querySelector('.checkbox__icon');
+const check_icon = document.querySelector('.checked__icon');
+const btn_sub_form = document.querySelector('.submit_form');
+
+check_box.addEventListener('click', () {
+   isChecked = input_checkbox.checked;
+   input_checkbox.checked = !isChecked;
+   switch (isChecked) {
+      case true:
+         check_icon.classList.remove('checked--active');
+         check_box.classList.add('checkbox__icon--disabled');
+         break
+      case false:
+         check_icon.classList.add('checked--active')
+         check_box.classList.remove('checkbox__icon--disabled');
+   }
+});
+
+
 
 
 
